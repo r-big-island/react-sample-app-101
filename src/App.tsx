c5d94c538e727cccd2d89c36880962c42161135f
r-big-island <lalaknockrara@gmail.com>
2022-04-30 18:14:32 +0900

    implement object state

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,29 @@ interface statesSampleType {
 }
 
 const App: React.FC<statesSampleType> = (props) => {
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState<number>(props.price);
+  const [state, setState] = useState(props);
+  // 分割代入を行うことで、state.~となっているところをリファクタリングできる
+  const { name, price } = state;
+  // const [name, setName] = useState(props.name);
+  // const [price, setPrice] = useState<number>(props.price);
 
-  const reset = () => {
-    setPrice(props.price);
-    setName(props.name);
-  }
+  const reset = () => setState(props); // {
+    // setPrice(props.price);
+    // setName(props.name);
+    // setState(props);
+  // }
   return (
     <>
       <p>現在の{name}は、{price}円です。</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
+      {/* <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button> */}
+      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
+      {/* <button onClick={reset}>Reset</button> */}
+      <button onClick={() => setState(props)}>Reset</button>
       {/* e.target.value: onChangeが発生したときにinput入力欄に入力されている値を拾う書き方（お約束的） */}
-      <input value={name} onChange={e => setName(e.target.value)} />
+      {/* <input value={state.name} onChange={e => setName(e.target.value)} /> */}
+      <input value={state.name} onChange={e => setState({...state, name: e.target.value})} />
     </>
   );
 }
