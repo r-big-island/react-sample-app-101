e3483c60ab6cb6f4343a8d58eb5ed74665f9db3e
r-big-island <lalaknockrara@gmail.com>
2022-05-03 13:18:19 +0900

    implement createContext the way before the hooks

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,8 +3,9 @@ import React, { useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import EventForm from './EventForm';
-
 import Events from './Events';
+// contextを利用するためのimport
+import AppContext from '../contexts/AppContext';
 // 作成したreducerを利用するためのimport
 import reducer from '../reducers';
 
@@ -26,11 +27,14 @@ const App: React.FC = () => {
   >(reducer, []);
 
   return (
-    // JSX記法では、通常のHTMLとは異なるルールでダグのプロパティを作成する必要がある
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch} />
-      <Events state={state} dispatch={dispatch} />
-    </div>
+    // React Hooksが導入されるより前の書き方
+    <AppContext.Provider value={'Hello, I am a Provider'}>
+      {/* JSX記法では、通常のHTMLとは異なるルールでダグのプロパティを作成する必要がある */}
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
+      </div>
+    </AppContext.Provider>
   );
 };
 
