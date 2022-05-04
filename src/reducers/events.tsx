7c9c7676a98a6fe898b36fc2eaa6d9498cbc8477
r-big-island <lalaknockrara@gmail.com>
2022-05-04 11:27:39 +0900

    create operationLogs reducer

--- src/reducers/events.tsx
+++ src/reducers/events.tsx
@@ -1,18 +1,6 @@
 import { CREATE_EVENT, DELETE_EVENT, DELETE_ALL_EVENTS } from '../actions';
 import { eventForm, eventState, appStates } from '../@types/orgTypes';
 
-// type eventForm = {
-//   type?: string;
-//   title?: string;
-//   body?: string;
-//   id?: number;
-// };
-// type eventState = {
-//   id: number;
-//   title?: string;
-//   body?: string;
-// };
-
 const events: (state: appStates, action: eventForm) => appStates = (
   state: appStates,
   action: eventForm
@@ -22,11 +10,17 @@ const events: (state: appStates, action: eventForm) => appStates = (
       const event = { title: action.title, body: action.body };
       const length: number = state.events.length;
       let id: number = length === 0 ? 1 : state.events[length - 1].id + 1;
-      return { events: [...state.events, { id, ...event }] };
+      return {
+        events: [...state.events, { id, ...event }],
+        operationLogs: state.operationLogs,
+      };
     case DELETE_EVENT:
-      return { events: state.events.filter((event) => event.id !== action.id) };
+      return {
+        events: state.events.filter((event) => event.id !== action.id),
+        operationLogs: state.operationLogs,
+      };
     case DELETE_ALL_EVENTS:
-      return { events: [] };
+      return { events: [], operationLogs: state.operationLogs };
     default:
       return state;
   }
