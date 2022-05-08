010d812278cf2d6057677be70498b7e89f6082b7
r-big-island <lalaknockrara@gmail.com>
2022-05-08 16:03:09 +0900

    save state with using localStorage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,29 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useEffect } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import EventForm from './EventForm';
 import Events from './Events';
-import store from '../store/store';
-import { Provider } from 'react-redux';
 import OperationLogs from './OperationLogs';
+import { useAppSelector } from '../app/hooks';
+import { APP_KEY } from '../actions';
 
 const App: React.FC = () => {
+  const state = useAppSelector((state) => state);
+  useEffect(() => {
+    localStorage.setItem(APP_KEY, JSON.stringify(state));
+  }, [state]);
   return (
     // React Hooksが導入されてからの書き方（Provider側は変更なし）
     // AppContext.Providerタグに囲まれた要素であれば、孫コンポーネントになってもvalueに渡されたデータをProvideすることができる
     // {/* JSX記法では、通常のHTMLとは異なるルールでダグのプロパティを作成する必要がある */}
-    <Provider store={store}>
-      <div className="container-fluid">
-        <EventForm />
-        <Events />
-        <OperationLogs />
-      </div>
-    </Provider>
+    // <Provider store={store}>
+    <div className="container-fluid">
+      <EventForm />
+      <Events />
+      <OperationLogs />
+    </div>
+    // </Provider>
   );
 };
 
